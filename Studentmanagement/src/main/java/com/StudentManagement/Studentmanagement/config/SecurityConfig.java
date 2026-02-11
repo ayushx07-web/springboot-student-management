328d0a5042b8f2958871fedeec366663e71c619a
ayushx07-web <kandpalayush4@gmail.com>
2026-02-11 16:59:11 +0530

    Fix login auth

--- Studentmanagement/src/main/java/com/StudentManagement/Studentmanagement/config/SecurityConfig.java
+++ Studentmanagement/src/main/java/com/StudentManagement/Studentmanagement/config/SecurityConfig.java
@@ -4,41 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    // PASSWORD ENCODER
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    public UserDetailsService userDetailsService() {
 
-    // USER
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(encoder.encode("admin123")) // encrypted
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin123")   // IMPORTANT
                 .roles("ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(admin);
     }
 
-    // SECURITY
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
